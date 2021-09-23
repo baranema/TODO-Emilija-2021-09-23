679c23558adbd7827c88c5bfaaab4136cffb3285
ebaranauskaite <e.b.baranauskaitema@student.vu.nl>
2021-09-23 13:44:44 +0300

    delete

--- app.js
+++ app.js
@@ -11,23 +11,6 @@ app.use(express.urlencoded());
 
 app.use(express.json());
 
-app.post('/', function(request, response){ 
-    let data = JSON.stringify(request.body.todo);
-
-    let rawdata = fs.readFileSync('./todos.json'); 
-
-    if (rawdata.length != 0) {
-        rawdata = rawdata + ", " + data;
-    } else {
-        rawdata = data; 
-    }
-    
-    fs.writeFileSync('./todos.json', rawdata);
-
-    let parsedJson = JSON.parse("[" + rawdata + "]");
-    response.render('index', { json: parsedJson });
-});  
-
 app.get('/', (req, res) => {  
     fs.readFile('./todos.json', 'utf8' , (err, data) => {
         if (err) {
@@ -38,3 +21,38 @@ app.get('/', (req, res) => {
         res.render('index', { json: parsedJson }); 
     });
 }); 
+
+
+app.put('/', (req, res) => { 
+    let passJson = JSON.stringify(req.body);
+    let substring = passJson.substring(1);
+    let newSubstring = substring.substring(0, substring.length - 1);
+     
+    fs.writeFileSync('./todos.json', newSubstring);
+
+    let parsedJson = JSON.parse("[" + newSubstring + "]");
+    res.render('index', { json: parsedJson });
+    
+    req.end();
+    res.end();
+}); 
+
+app.post('/', function(request, response){ 
+    let data = JSON.stringify(request.body.todo);
+
+    let rawdata = fs.readFileSync('./todos.json'); 
+
+    if (rawdata.length != 0) {
+        rawdata = rawdata + "," + data;
+    } else {
+        rawdata = data; 
+    }
+    
+    fs.writeFileSync('./todos.json', rawdata);
+
+    let parsedJson = JSON.parse("[" + rawdata + "]");
+    response.render('index', { json: parsedJson });
+
+    request.end();
+    response.end();
+});  
